5a9b94d471203fee355cfb58f9bfbe2be5225817
Serhey Dolgushev <dolgushev.serhey@gmail.com>
2013-02-11 10:08:13 +1300

    log for requests

--- classes/gateway.php
+++ classes/gateway.php
@@ -45,6 +45,10 @@ class DPSPaymentExpressRedirectGateway extends eZRedirectGateway
 	}
 
 	public static function sendRequest( $xml ) {
+		$log = fopen( 'var/log/dps_requests.log', 'a' );
+
+		fwrite( $log, str_repeat( '=', 80 ) . "\n" . 'Request:' . "\n" . $xml . "\n" );
+
 		$ch = curl_init();
 		curl_setopt( $ch, CURLOPT_URL, 'https://sec.paymentexpress.com/pxpay/pxaccess.aspx' );
 		curl_setopt( $ch, CURLOPT_POST, true );
@@ -52,6 +56,12 @@ class DPSPaymentExpressRedirectGateway extends eZRedirectGateway
 		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
 		curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
 		$r = curl_exec( $ch );
+		if( $r === false ) {
+			fwrite( $log, 'Error:' . "\n" . curl_error( $ch ) . "\n" );
+		} else {
+			fwrite( $log, 'Response:' . "\n" . $r . "\n" );
+		}
+		fclose( $log );
 		curl_close( $ch );
 		return (array) new SimpleXMLElement( $r );
 	}
